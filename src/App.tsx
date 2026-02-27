94ee60e865e5cb3cd12599f3ace479e8cc97f89a
Dilip Das <203666926+Dilipdasmnayaka@users.noreply.github.com>
2026-02-27 12:53:14 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen w-screen flex items-center justify-center p-0 m-0 bg-amber-100/30 overflow-hidden">
+        <div className=' h-[30rem] w-[30rem] bg-white rounded-lg shadow-lg shadow-purple-500/30 flex items-center justify-center text-2xl font-bold  flex-col gap-2'>
+
+          <h1 className='text-purple-500 font-bold text-2xl'>Dilip Das M Nayaka</h1>
+          <h5 className='text-gray-500'>4NI23IS053</h5>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
